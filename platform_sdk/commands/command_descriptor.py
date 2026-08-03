277e830cda82c3a0b16ac6fce3b16ad713dcfd6d
Manuel De Mas <manuel.demas@designcoaching.net>
2026-08-03 15:18:14 +0200

    refactor: enhance Field descriptions in EventDescriptor and CommandDescriptor

--- platform_sdk/commands/command_descriptor.py
+++ platform_sdk/commands/command_descriptor.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class EventDescriptor(BaseModel):
@@ -8,14 +8,17 @@ class EventDescriptor(BaseModel):
     Describe an event that can be emitted by the Platform.
     """
 
-    name: str
-    """
-    The name of the event.
-    """
-    payload: type[BaseModel]
-    """
-    The payload schema of the event.
-    """
+    name: str = Field(
+        ...,
+        description="The name of the event.",
+    )
+    payload: Optional[type[BaseModel]] = Field(
+        default=None,
+        description=(
+            "The schema of the event payload. "
+            "If None, the event does not have a payload."
+        ),
+    )
 
 
 class CommandDescriptor(BaseModel):
@@ -23,20 +26,29 @@ class CommandDescriptor(BaseModel):
     Describe a command that can be sent to the Platform.
     """
 
-    name: str
-    """
-    The name of the command.
-    """
-    payload: type[BaseModel]
-    """
-    The payload schema of the command.
-    """
-    result: type[BaseModel]
-    """
-    The result schema of the command.
-    """
-    exceptions: Optional[list[type[Exception]]] = None
-    """
-    The exceptions that can be raised when executing the command.
-    None means no exceptions are expected.
-    """
+    name: str = Field(
+        ...,
+        description="The name of the command.",
+    )
+    payload: Optional[type[BaseModel]] = Field(
+        default=None,
+        description=(
+            "The schema of the command payload. "
+            "If None, the command does not require a payload."
+        ),
+    )
+    result: Optional[type[BaseModel]] = Field(
+        default=None,
+        description=(
+            "The schema of the command result. "
+            "If None, the command does not return a result."
+        ),
+    )
+    exceptions: Optional[list[type[Exception]]] = Field(
+        default=None,
+        description=(
+            "The exceptions that can be raised when executing "
+            "the command. "
+            "If None, the command does not raise any exception."
+        ),
+    )
